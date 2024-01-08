98ab420cf1ba4d5924f51d90cd36aa53cac7d821
Pseudomata <pseudomata@proton.me>
2024-01-08 12:32:58 -0500

    Fix the dev shell

--- flake.nix
+++ flake.nix
@@ -1,22 +1,23 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-        let
-          pkgs = import nixpkgs { inherit system; };
-        in
-        {
-          devShell = with pkgs; mkShell {
-            buildInputs = [ 
-              cargo
-              rustc
-              rustfmt
-            ];
+      let
+        pkgs = import nixpkgs { inherit system; };
+        inherit (pkgs.darwin.apple_sdk.frameworks) Security;
+      in {
+        devShell = with pkgs;
+          mkShell {
+            buildInputs =
+              [ cargo clippy rustc rustfmt rust-analyzer libiconv pkg-config ]
+              ++ lib.optionals stdenv.isDarwin [ Security ];
+            shellHook = ''
+              echo && echo && echo "Entering dev shell for 'tree-sitter-grammars' project."
+            '';
           };
-        }
-      );
+      });
 }
